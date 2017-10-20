a7a9ebbbd330dc29cc90511813a2c7760e53a65a
OlegSokol <soklakov.o.yu@gmail.com>
2017-10-20 22:37:35 +0300

    add json router, add tests

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,38 +1,50 @@
 package com.example.demo;
 
-import static org.springframework.web.reactive.function.BodyInserters.fromObject;
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
-import java.io.IOException;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.Data;
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
 import reactor.ipc.netty.http.server.HttpServer;
 
-@SpringBootApplication
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Data
+class Hello {
+    private final String name;
+}
+
 public class DemoApplication {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+    static RouterFunction getRouter() {
+        HandlerFunction hello = request -> ok().body(fromObject("Hello"));
+        return route(
+                    GET("/"), hello)
+                .andRoute(
+                    GET("/json"), req ->
+                        ok()
+                        .contentType(APPLICATION_JSON)
+                        .body(fromObject(new Hello("world"))));
+    }
 
-		HandlerFunction hello = request -> ServerResponse.ok().body(fromObject("Hello"));
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-		RouterFunction router = route(GET("/"), hello);
+        RouterFunction router = getRouter();
 
-		HttpHandler httpHandler = RouterFunctions.toHttpHandler(router);
+        HttpHandler httpHandler = RouterFunctions.toHttpHandler(router);
 
-		HttpServer
-				.create("localhost", 8080)
-				.newHandler(new ReactorHttpHandlerAdapter(httpHandler))
-				.block();
+        HttpServer
+                .create("localhost", 8080)
+                .newHandler(new ReactorHttpHandlerAdapter(httpHandler))
+                .block();
 
-		Thread.currentThread().join();
-	}
+        Thread.currentThread().join();
+    }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,28 @@
 package com.example.demo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class DemoApplicationTests {
 
+	private final WebTestClient webTestClient = WebTestClient.bindToRouterFunction(DemoApplication.getRouter()).build();
+
 	@Test
-	public void contextLoads() {
+	public void getTest() {
+		webTestClient.get().uri("/").exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(String.class)
+				.isEqualTo("Hello");
 	}
 
+	@Test
+	public void jsonTest() {
+		webTestClient.get().uri("/json").exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody(Hello.class)
+				.isEqualTo(new Hello("world"));
+
+	}
 }
